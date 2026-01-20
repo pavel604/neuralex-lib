633c3ae646450d3863cdf0acd497ee228162ff85
Pavel <pavel604@users.noreply.github.com>
2026-01-19 22:24:44 -0800

    feat: Add EmbeddingInputData support for BYOE mode (v0.2.0)

--- python/neuralex/__init__.py
+++ python/neuralex/__init__.py
@@ -13,7 +13,7 @@ from .models import (
 )
 from .exceptions import NeuraLexError, AuthenticationError, RateLimitError, APIError
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = [
     "NeuraLexClient",
     "AsyncNeuraLexClient",
